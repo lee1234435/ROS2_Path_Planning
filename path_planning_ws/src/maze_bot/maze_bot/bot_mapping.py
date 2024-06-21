b488145c5ee57ecd994a1b7499eb223873ab8886
Lee Seung Ho <133578714+lee1234435@users.noreply.github.com>
2024-06-21 10:05:25 +0900

    Update bot_mapping.py

--- path_planning_ws/src/maze_bot/maze_bot/bot_mapping.py
+++ path_planning_ws/src/maze_bot/maze_bot/bot_mapping.py
@@ -1,28 +1,3 @@
-'''
-> Purpose :
-Module to perform mapping to convert [(top down) maze view ==> traversable graph.]
-
-> Usage :
-You can perform mapping by
-1) Importing the class (bot_mapper)
-2) Creating its object
-3) Accessing the object's function of (graphify). 
-E.g ( self.bot_mapper.graphify(self.bot_localizer.maze_og) )
-
-
-> Inputs:
-1) Occupancy Grid from localization stage
-
-> Outputs:
-1) self.Graph.graph       => Generated graph from provided maze occupancy grid
-2) self.maze              => Image displaying only pathways in maze
-
-Author :
-Haider Abbasi
-
-Date :
-6/04/22
-'''
 import cv2
 import numpy as np
 
